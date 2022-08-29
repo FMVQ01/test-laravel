4fd8355cc10725ed1d18ad3cc07a81d0ee20ecbf
FMVQ01 <yu022713@outlook.jp>
2022-08-29 05:52:02 +0000

    修正

---
+++ app/Http/Controllers/ShopController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Stock; //追加
+
+use Illuminate\Http\Request;
+
+class ShopController extends Controller
+{
+    public function index() //追加
+    {
+        $stocks = Stock::Paginate(6); //Eloquantで検索
+        return view('shop',compact('stocks')); 
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShopController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ShopController::class, 'index']);
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
